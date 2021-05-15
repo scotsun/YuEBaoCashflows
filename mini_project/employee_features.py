790d308863a0e6d51babd410ddfab59f18ca6b06
scotsun <minghuisunscott@gmail.com>
2021-05-14 21:12:52 -0400

    mini project + cassandra lab report

--- mini_project/employee_features.py
+++ mini_project/employee_features.py
@@ -1,38 +1,164 @@
+from random import randint
+
+
 def clear_entries(entries):
     for entry in entries:
         entry.delete(0, "end")
     return
 
 
-def get_latest_cid(session):
-    rows = session.execute('SELECT * from parking_lot')
-    if not rows:
-        print('no matched record')
-        return 0
-    else:
-        rows = list(rows)
-        return len(rows)
+def get_cid():
+    return randint(1, 1000)
 
 
-def add_car(name_entry, position_entry, car_make_entry, car_model_entry, session):
-    entries = [name_entry, position_entry, car_make_entry, car_model_entry]
-    name = name_entry.get()
+def add_car(uname_entry, position_entry, car_make_entry, car_model_entry, session):
+    entries = [uname_entry, position_entry, car_make_entry, car_model_entry]
+    uname = uname_entry.get()
     position = position_entry.get()
     car_make = car_make_entry.get()
     car_model = car_model_entry.get()
 
     # check for mis-info
-    if any(elem == "" for elem in [name, position, car_make, car_model]):
+    if any(elem == "" for elem in [uname, position, car_make, car_model]):
         print("you missed some information; plz try again")
         clear_entries(entries)
         return
-    car_id = get_latest_cid(session) + 1
-    cql = f"INSERT INTO parking_lot (employee_name, cid, car_make, car_model, position) \
-            VALUES ('{name}', {car_id}, '{car_make}', '{car_model}', '{position}') \
+
+    car_id = get_cid()
+    cql = f"INSERT INTO parking_lot (employee_uname, cid, car_make, car_model, position) \
+            VALUES ('{uname}', {car_id}, '{car_make}', '{car_model}', '{position}') \
             if not exists"
     session.execute(cql)
     print("a new car cid:", car_id, "is added to the parking lot management system")
     clear_entries(entries)
     return
 
-# def del
+
+def del_car(uname_entry, car_id_entry, session):
+    entries = [uname_entry, car_id_entry]
+    uname = uname_entry.get()
+    cid = car_id_entry.get()
+
+    # check for mis-info
+    if any(elem == "" for elem in [uname, cid]):
+        print("you missed some information; plz try again")
+        clear_entries(entries)
+        return
+    try:
+        cid = int(cid)
+    except ValueError:
+        print("incorrect form of car id")
+        clear_entries(entries)
+        return
+    # check if exist
+    cid_row = session.execute(f"SELECT * from parking_lot where uname='{uname}' and cid={cid}")
+    if not cid_row:
+        clear_entries(entries)
+        print("this book does not exist")
+        return
+
+    cql = f"DELETE from parking_lot where employee_uname='{uname}' and cid={cid}"
+    session.execute(cql)
+    print(f"car: {cid} deleted")
+    clear_entries(entries)
+    return
+
+
+def edit_car(uname_entry, car_id_entry, position_entry, car_make_entry, car_model_entry, session):
+    entries = [uname_entry, car_id_entry, position_entry, car_make_entry, car_model_entry]
+    uname = uname_entry.get()
+    cid = car_id_entry.get()
+    position = position_entry.get()
+    car_make = car_make_entry.get()
+    car_model = car_model_entry.get()
+
+    # check for mis-info
+    if any(elem == "" for elem in [uname, cid, position, car_make, car_model]):
+        print("you missed some information; plz try again")
+        clear_entries(entries)
+        return
+    try:
+        cid = int(cid)
+    except ValueError:
+        print("incorrect form of car id")
+        clear_entries(entries)
+        return
+    # check if exist
+    cid_row = session.execute(f"SELECT * from parking_lot where employee_uname='{uname}' and cid={cid}")
+    if not cid_row:
+        clear_entries(entries)
+        print("this book does not exist")
+        return
+
+    cql = f"update parking_lot set position = '{position}', car_make = '{car_make}',  \
+            car_model = '{car_model}' where employee_uname = '{uname}' and cid = {cid};"
+    session.execute(cql)
+    print(f"car: {cid} updated")
+    clear_entries(entries)
+    return
+
+
+def search_car(uname_entry, car_id_entry, position_entry, car_make_entry, car_model_entry, session):
+    entries = [uname_entry, car_id_entry, position_entry, car_make_entry, car_model_entry]
+    uname = uname_entry.get()
+    cid = car_id_entry.get()
+    position = position_entry.get()
+    car_make = car_make_entry.get()
+    car_model = car_model_entry.get()
+
+    try:
+        if cid == "":
+            cid = -1
+        else:
+            cid = int(cid)
+    except ValueError:
+        print("incorrect form of car id")
+        clear_entries(entries)
+        return
+    inputs = [uname, cid, position, car_make, car_model]
+    input_names = ['employee_name', 'cid', 'position', 'car_make', 'car_model']
+    # check for mis-info
+    if all(elem == "" for elem in inputs):
+        print("you missed some information; plz try again")
+        clear_entries(entries)
+        return
+
+    values = []
+    where = input_names.copy()
+    for i in range(len(inputs)):
+        if inputs[i] == "":
+            where.remove(input_names[i])
+        else:
+            values.append(inputs[i])
+    cql = "select * from parking_lot "
+    cql = cql + "where "
+    for i in range(len(where) - 1):
+        if where[i] != 'cid':
+            add = f"{where[i]} = '{values[i]}' and "
+        else:
+            add = f"{where[i]} = {values[i]} and "
+        cql = cql + add
+    n = len(where) - 1
+    if where[n] != 'cid':
+        add = f"{where[n]} = '{values[n]}'"
+    else:
+        add = f"{where[n]} = {values[n]}"
+    cql = cql + add
+    print(cql)
+    rows = session.execute(cql)
+    if not rows:
+        print("no matched record")
+    for row in rows:
+        print(row)
+    clear_entries(entries)
+    return
+
+
+def list_all_cars(session):
+    cql = f"select * from parking_lot"
+    rows = session.execute(cql)
+    if not rows:
+        print("no car is registered yet")
+    print("registered cars:")
+    for row in rows:
+        print(row)
--- mini_project/gui.py
+++ mini_project/gui.py
@@ -1,6 +1,7 @@
 import tkinter
 from tkinter import ttk
 from cassandra.cluster import Cluster
+from cassandra.query import dict_factory
 from ssh import *
 from employee_features import *
 
@@ -10,7 +11,7 @@ def main():
     tunnel.start()
     cluster = Cluster(['34.67.124.229'])
     session = cluster.connect('test_keyspace')
-
+    session.row_factory = dict_factory
     root = tkinter.Tk()
     root.title("Parking lot System")
     make_employees_frame(root, session)
@@ -23,43 +24,45 @@ def make_employees_frame(root, session):
     v = tkinter.StringVar()
 
     # input entries
-    name_label = ttk.Label(employees_frame, text="Name")
-    name_entry = ttk.Entry(employees_frame, width=8)
+    uname_label = ttk.Label(employees_frame, text="UName")
+    uname_entry = ttk.Entry(employees_frame, width=8)
     position_label = ttk.Label(employees_frame, text="Position")
     position_entry = ttk.Entry(employees_frame, width=8)
     car_make_label = ttk.Label(employees_frame, text="Car Make")
     car_make_entry = ttk.Entry(employees_frame, width=8)
     car_model_label = ttk.Label(employees_frame, text="Car Model")
     car_model_entry = ttk.Entry(employees_frame, width=8)
+    cid_label = ttk.Label(employees_frame, text="Car ID")
+    cid_entry = ttk.Entry(employees_frame, width=8)
 
-    name_label.grid(row=0, column=0)
-    name_entry.grid(row=0, column=1)
+    uname_label.grid(row=0, column=0)
+    uname_entry.grid(row=0, column=1)
     position_label.grid(row=0, column=2)
     position_entry.grid(row=0, column=3)
     car_make_label.grid(row=1, column=0)
     car_make_entry.grid(row=1, column=1)
     car_model_label.grid(row=1, column=2)
     car_model_entry.grid(row=1, column=3)
+    cid_label.grid(row=2, column=0)
+    cid_entry.grid(row=2, column=1)
 
     # add buttons
     add_employee_button = ttk.Button(employees_frame, text="add car")
     del_employee_button = ttk.Button(employees_frame, text="delete car")
     edit_employee_button = ttk.Button(employees_frame, text="edit car")
-    search_employee_button = ttk.Button(employees_frame, text="search car")
-    list_all_employees_button = ttk.Button(employees_frame, text="all employees' cars")
+    list_all_employees_button = ttk.Button(employees_frame, text="all cars")
 
     add_employee_button.grid(row=3, column=0)
     del_employee_button.grid(row=3, column=1)
     edit_employee_button.grid(row=3, column=2)
-    search_employee_button.grid(row=3, column=3)
-    list_all_employees_button.grid(row=4, column=0)
+    list_all_employees_button.grid(row=3, column=3)
 
-    add_employee_button['command'] = lambda: add_car(name_entry, position_entry, car_make_entry,
-                                                     car_model_entry, session)
-    # add_employee_button['command'] = lambda: del_employee(car_make_entry, session)
-    # add_employee_button['command'] = lambda: edit_employee(name_entry, id_entry, car_make_entry,
-    #                                                        car_model_entry, session)
-    # add_employee_button['command'] = lambda: search_employee(name_entry, id_entry, car_make_entry, session)
+    add_employee_button['command'] = lambda: add_car(uname_entry, position_entry, car_make_entry, car_model_entry,
+                                                     session)
+    del_employee_button['command'] = lambda: del_car(uname_entry, cid_entry, session)
+    edit_employee_button['command'] = lambda: edit_car(uname_entry, cid_entry, position_entry, car_make_entry,
+                                                       car_model_entry, session)
+    list_all_employees_button['command'] = lambda: list_all_cars(session)
     return
 
 
